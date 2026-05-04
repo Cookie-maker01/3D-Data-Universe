01100cb972605a3f42cc4123506477c483fdb8e8
unknown <weifang060@gmail.com>
2026-05-04 17:26:30 +1200

    Modify nodes and app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,28 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import './App.css';
 
-function Sphere() {
-  return (
-    <mesh>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color="#4f8cff" />
-    </mesh>
-  );
-}
+import Planet from "./components/Planet";
+import { nodes } from "./data/nodes";
+
 
 export default function App() {
   return (
     <div className="app">
-      <Canvas camera={{ position: [0, 0, 5], fov: 60}}>
+      <Canvas camera={{ position: [0, 0, 6], fov: 60}}>
 
-        <ambientLight intensity={0.5} />
+        <ambientLight intensity={0.6} />
         <pointLight position={[10, 10, 10]} />
 
-        <Stars radius={100} depth={50} count={5000} factor={4} />
+        <Stars radius={100} depth={50} count={5000} factor={4} fade/>
 
-        <Sphere />
+        {nodes.map((node) => (
+          <Planet
+            key={node.id}
+            position={node.position as [number, number, number]}
+            color={node.color}
+            size={node.size}
+          />
+        ))}
 
         <OrbitControls enableDamping />
 
--- src/data/nodes.ts
+++ src/data/nodes.ts
@@ -1,4 +1,11 @@
-export const nodes = [
+export type Node = {
+  id:string;
+  position: [number, number, number];
+  color: string;
+  size: number;
+}
+
+export const nodes: Node[] = [
   { id: "projects", position: [3, 0, 0], color: "#4f8cff", size: 1},
   { id: "skills", position: [-3, 1, 0], color: "#00ffcc", size: 0.8},
   { id: "activity", position: [0, 2, -2], color: "#ff66cc", size: 0.7},
